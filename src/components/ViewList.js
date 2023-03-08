35bb639a027549ac9ce5478489ca0945b7ed6287
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2023-03-08 18:35:02 +0300

    finalize the list view of products page

--- src/components/ViewList.js
+++ src/components/ViewList.js
@@ -1,8 +1,5 @@
-
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-
-import { truncate } from "../utils/utils";
 
 const ViewList = ({ products }) => {
   return (
@@ -25,11 +22,17 @@ const ViewList = ({ products }) => {
                 </div>
                 <img className="img" src={thumbnail} alt={title} />
               </div>
-              <div className="text-container">
+              <div className="info-container">
                 <h5>{product.title}</h5>
-                <p>${price}</p>
-                <p>{truncate(description, 100)}</p>
-                <Link className="btn btn-variant" to={`/products/${id}`}>view details</Link>
+                <p className="price">${price}</p>
+                <div className="description-container">
+                  <p>{description}</p>
+                </div>
+                <div className="btn-container">
+                  <Link className="btn btn-variant" to={`/products/${id}`}>
+                    view details
+                  </Link>
+                </div>
               </div>
             </article>
           );
@@ -49,16 +52,16 @@ const StyledList = styled.div`
       display: grid;
       height: 100%;
       align-items: center;
-      column-gap: 2rem;
     }
   }
+
   .image-container {
     height: 100%;
     max-height: 200px;
-    margin-bottom: 1rem;
 
     overflow: hidden;
-    border-radius: var(--borderRadius);
+    border-top-left-radius: var(--borderRadius);
+    border-top-right-radius: var(--borderRadius);
 
     position: relative;
     .img {
@@ -66,22 +69,54 @@ const StyledList = styled.div`
     }
   }
 
-  .text-container {
-    /* padding: 0.5rem; */
+  .info-container {
+    display: grid;
     height: 100%;
+    padding: var(--element-padding);
+    align-items: center;
+
+    letter-spacing: var(--letterSpacing);
+
+    border-bottom-left-radius: var(--borderRadius);
+    border-bottom-right-radius: var(--borderRadius);
+    background-color: var(--grey-200);
     overflow: hidden;
 
-    p {
-      margin-bottom: 0.5rem;
-      font-size: var(--fontSizeTextSmall);
-    }
     h5 {
-      margin-bottom: 0.5rem;
+      margin-bottom: 0;
 
-      color: var(--secondary-900);
-      letter-spacing: var(--letterSpacing);
+      color: var(--secondary-800);
       font-weight: 600;
       font-size: 1rem;
+    }
+
+    .price {
+      color: var(--secondary-700);
+      font-weight: 600;
+      font-size: 1.25rem;
+      margin-bottom: 0.3rem;
+    }
+
+    .description-container {
+      line-height: 1.5;
+      font-size: var(--fontSizeTextSmall);
+
+      p {
+        margin-bottom: 0;
+      }
+    }
+
+    .btn-container {
+      text-align: right;
+      margin-top: 0.375rem;
+    }
+
+    .btn {
+      box-shadow: none;
+      line-height: normal;
+      &:hover {
+        box-shadow: none;
+      }
     }
   }
 
@@ -117,32 +152,75 @@ const StyledList = styled.div`
     .image-container {
       max-height: 230px;
       margin-bottom: 0;
+
+      border-bottom-left-radius: var(--borderRadius);
+      border-top-right-radius: 0;
     }
+
     .products-list {
       grid-auto-rows: 170px;
+
       article {
-        grid-template-columns: minmax(190px, 300px) 50%;
+        grid-template-columns: minmax(170px, 300px) 60%;
+      }
+    }
+    .info-container {
+      grid-template-rows: min-content min-content minmax(35px, 1fr) min-content;
+
+      border-top-right-radius: var(--borderRadius);
+      border-bottom-left-radius: 0;
+
+      .price,
+      h5 {
+        margin-bottom: 0;
+      }
+
+      .description-container {
+        align-self: start;
+        height: 100%;
+        overflow: hidden;
       }
     }
   }
   @media (min-width: 650px) {
     .products-list {
-      grid-auto-rows: 180px;
+      grid-auto-rows: 190px;
+
       article {
-        grid-template-columns: minmax(230px, 300px) 50%;
+        grid-template-columns: minmax(190px, 300px) 60%;
       }
     }
   }
   @media (min-width: 850px) {
     .products-list {
       article {
-        grid-template-columns: minmax(230px, 330px) 50%;
+        grid-template-columns: minmax(230px, 330px) 60%;
       }
     }
   }
   @media (min-width: 992px) {
     .products-list {
       grid-auto-rows: 230px;
+    }
+
+    .info-container {
+      padding-left: 1.5rem;
+
+      h5 {
+        font-size: 1.25rem;
+      }
+
+      .price {
+        font-size: 1.5rem;
+      }
+
+      .btn-container {
+        text-align: left;
+
+        .btn {
+          line-height: inherit;
+        }
+      }
     }
   }
 `;
